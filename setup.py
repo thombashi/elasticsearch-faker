7da7accb757ff18a443a29a037dd53f89b05acad
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2022-01-30 21:38:15 +0900

    Update Development Status

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setuptools.setup(
         "test": TESTS_REQUIRES,
     },
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: Information Technology",
         "License :: OSI Approved :: MIT License",
