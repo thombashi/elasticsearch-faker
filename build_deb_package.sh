30689fb2a907209ef1fd0f97bca17c7df89b8212
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2022-03-05 21:48:11 +0900

    Refactor a build script

--- build_deb_package.sh
+++ build_deb_package.sh
@@ -10,7 +10,7 @@ BUILD_DIR_PATH="${TOPLEVEL_DIR}/${DPKG_BUILD_DIR}/${INSTALL_DIR_PATH}"
 PKG_NAME="elasticsearch-faker"
 PKG_NAME_SNAKE="elasticsearch_faker"
 PYTHON=python3
-MACHINE=$($PYTHON -c "import platform; machine=platform.machine().casefold(); print('amd64' if machine == 'x86_64' else machine)")
+ARCH=$(dpkg --print-architecture)
 
 cd "$TOPLEVEL_DIR"
 
@@ -40,7 +40,7 @@ cat << _CONTROL_ > "${DPKG_BUILD_DIR}/DEBIAN/control"
 Package: $PKG_NAME
 Version: $PKG_VERSION
 Maintainer: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
-Architecture: $MACHINE
+Architecture: $ARCH
 Description: $PKG_NAME is a CLI tool to generate fake data for Elasticsearch.
 Homepage: https://github.com/thombashi/$PKG_NAME
 Priority: extra
@@ -50,12 +50,12 @@ cat "${DPKG_BUILD_DIR}/DEBIAN/control" 2>&1
 VERSION_CODENAME=$(\grep -Po "(?<=VERSION_CODENAME=)[a-z]+" /etc/os-release)
 
 fakeroot dpkg-deb --build "$DPKG_BUILD_DIR" "$DIST_DIR_NAME"
-rename -v "s/_${MACHINE}.deb/_${VERSION_CODENAME}_${MACHINE}.deb/" ${DIST_DIR_NAME}/*
+rename -v "s/_${ARCH}.deb/_${VERSION_CODENAME}_${ARCH}.deb/" ${DIST_DIR_NAME}/*
 
 # generate an archive file
 ARCHIVE_EXTENSION=tar.gz
 SYSTEM=$($PYTHON -c "import platform; print(platform.system().casefold())")
-ARCHIVE_FILE="${PKG_NAME}_${PKG_VERSION}_${SYSTEM}_${VERSION_CODENAME}_${MACHINE}.${ARCHIVE_EXTENSION}"
+ARCHIVE_FILE="${PKG_NAME}_${PKG_VERSION}_${SYSTEM}_${VERSION_CODENAME}_${ARCH}.${ARCHIVE_EXTENSION}"
 
 cd "$BUILD_DIR_PATH"
 tar -zcvf "$ARCHIVE_FILE" "$PKG_NAME"
